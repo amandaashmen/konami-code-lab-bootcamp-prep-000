7332a1ceee6cdd50ab0ec8318e6ce9f3eb58c4bf
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-18 00:48:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -36,3 +36,24 @@ function init() {
       }
   }
 }
+
+function init() {
+  // your code here	  //
+  var index = 0
+  document.body.addEventListener('keydown', onKeyDownHandler);
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log('key', key)
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length) {
+        alert("YOU DID IT!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
+}	
