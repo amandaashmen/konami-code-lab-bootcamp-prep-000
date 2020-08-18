a5c056058ea5183d8fe0420a339b333aa7b69c59
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-18 00:49:32 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -46,6 +46,7 @@ function init() {
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which)
     console.log('key', key)
+    console.log('test', e.key)
     if (key === code[index]) {
       index++
 
