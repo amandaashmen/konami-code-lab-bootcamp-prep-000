f1ae87b2fe8110e90621c38d2360a9bd8c0622e2
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-18 00:42:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,7 @@ const codes = [
 function init() {
   // Attaching an keydown event listener to document.body
   
-  var index = 0;
+  var index = 0
   
   document.body.addEventListener('keydown', onKeyDownHandler);
   
@@ -24,17 +24,17 @@ function init() {
     
     // Now, how can we check for which specific key was pressed?
   
-    const key = parseInt(e.detail || e.which);
+    const key = parseInt(e.detail || e.which || e.key);
     
     if (key === codes[index]) {
-      index++;
+      index++
  
       if (index === codes.length-1) {
-        alert("Hurray!");
-        index = 0;
+        alert("Hurray!")
+        index = 0
       }
     } else {
-      index = 0;
+      index = 0
       }
   }
 }
